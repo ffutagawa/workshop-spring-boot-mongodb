2f85373adbae61faf86662074c04309960e75e4b
FFutagawa <ffutagawa@gmail.com>
2024-06-15 16:43:01 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/ffutagawa/workshopmongodb/config/Instantiation.java
+++ src/main/java/com/ffutagawa/workshopmongodb/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.ffutagawa.workshopmongodb.domain.Post;
 import com.ffutagawa.workshopmongodb.domain.User;
 import com.ffutagawa.workshopmongodb.dto.AuthorDTO;
+import com.ffutagawa.workshopmongodb.dto.CommentDTO;
 import com.ffutagawa.workshopmongodb.repository.PostRepository;
 import com.ffutagawa.workshopmongodb.repository.UserRepository;
 
@@ -40,6 +41,13 @@ public class Instantiation implements CommandLineRunner{
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "partiu viagem", "Vou viajar para São Paulo", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei Feliz hoje", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem !!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveita!", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
